2eaabb20a24bf6a86c5862ec63b6152e7f5cc6ec
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-08 20:32:38 +0900

    Do /basics/7 (Named return values)

--- tour.go
+++ tour.go
@@ -8,6 +8,12 @@ func add(x, y int) int {
 	return x + y
 }
 
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func swap(x, y string) (string, string) {
 	return y, x
 }
@@ -15,4 +21,5 @@ func swap(x, y string) (string, string) {
 func main() {
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
+	fmt.Println(split(17))
 }
