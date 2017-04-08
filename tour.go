1087648c835003e12f57f940cc37ab918014d2a2
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-08 20:36:01 +0900

    Do /basics/9 (Variables with initializers)

--- tour.go
+++ tour.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-var c, python, java bool
+var i, j int = 1, 2
 
 func add(x, y int) int {
 	return x + y
@@ -25,6 +25,6 @@ func main() {
 	fmt.Println(a, b)
 	fmt.Println(split(17))
 
-	var i int
-	fmt.Println(i, c, python, java)
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
 }
