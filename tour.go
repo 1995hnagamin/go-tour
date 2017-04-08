a37d39e18768e3f6e4e2524e745abb8a09889b25
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-08 20:27:23 +0900

    Do /basics/4 (functions)

--- tour.go
+++ tour.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println(add(42, 13))
 }
